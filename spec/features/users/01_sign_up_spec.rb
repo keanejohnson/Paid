dad5ade4864c178c68022515e8548ce705e38ca7
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-18 15:46:14 -0400

    fixed hound errors

--- spec/features/users/01_sign_up_spec.rb
+++ spec/features/users/01_sign_up_spec.rb
@@ -1,6 +1,6 @@
-require "rails_helper"
+require 'rails_helper'
 
-feature "user signs up", %{
+feature 'user signs up', %{
 As a prospective user
 I want to create an account
 So that I can use the application
@@ -15,63 +15,63 @@ So that I can use the application
 # * If I didn't enter valid information, I receive an error message and I am
 #   left unauthenticated
 
-  context "as a prospective user" do
-    scenario "I can visit the root path and click a link to create a new
-      account" do
+  context 'as a prospective user' do
+    scenario 'I can visit the root path and click a link to create a new
+      account' do
         visit root_path
-        click_link "Sign Up"
+        click_link 'Sign Up'
 
-        expect(page).to have_css("form#new_user")
+        expect(page).to have_css('form#new_user')
     end
 
-    scenario "I am taken to the new user form after I click the link to create
-      a new account" do
+    scenario 'I am taken to the new user form after I click the link to create
+      a new account' do
         visit root_path
-        click_link "Sign Up"
+        click_link 'Sign Up'
 
-        expect(page).to have_css("input#user_first_name")
-        expect(page).to have_css("input#user_last_name")
-        expect(page).to have_css("input#user_email")
-        expect(page).to have_css("input#user_password")
-        expect(page).to have_css("input#user_password_confirmation")
+        expect(page).to have_css('input#user_first_name')
+        expect(page).to have_css('input#user_last_name')
+        expect(page).to have_css('input#user_email')
+        expect(page).to have_css('input#user_password')
+        expect(page).to have_css('input#user_password_confirmation')
     end
 
-    scenario "specifying valid and required information allows me to
-      successfully create an account" do
+    scenario 'specifying valid and required information allows me to
+      successfully create an account' do
         visit root_path
-        click_link "Sign Up"
-        fill_in "First Name", with: "John"
-        fill_in "Last Name", with: "Smith"
-        fill_in "Email", with: "john.smith@example.com"
-        fill_in "user_password", with: "password"
-        fill_in "Password Confirmation", with: "password"
-        click_button "Sign Up"
+        click_link 'Sign Up'
+        fill_in 'First Name', with: 'John'
+        fill_in 'Last Name', with: 'Smith'
+        fill_in 'Email', with: 'john.smith@example.com'
+        fill_in 'user_password', with: 'password'
+        fill_in 'Password Confirmation', with: 'password'
+        click_button 'Sign Up'
 
-        expect(page).to have_content("You have successfully signed up!")
-        expect(page).to have_content("Sign Out")
+        expect(page).to have_content('You have successfully signed up!')
+        expect(page).to have_content('Sign Out')
     end
 
-    scenario "specifying an invalid email address to create an account will
-      re-render the page with an error message" do
+    scenario 'specifying an invalid email address to create an account will
+      re-render the page with an error message' do
       visit root_path
-      click_link "Sign Up"
-      click_button "Sign Up"
+      click_link 'Sign Up'
+      click_button 'Sign Up'
 
       expect(page).to have_content("can't be blank")
-      expect(page).to_not have_content("Sign Out")
+      expect(page).to_not have_content('Sign Out')
     end
 
-    scenario "specifying a password confirmation that does not match password
-      will re-render the page with an error message" do
+    scenario 'specifying a password confirmation that does not match password
+      will re-render the page with an error message' do
       visit root_path
-      click_link "Sign Up"
+      click_link 'Sign Up'
 
-      fill_in "user_password", with: "password"
-      fill_in "Password Confirmation", with: "somethingdifferent"
-      click_button "Sign Up"
+      fill_in 'user_password', with: 'password'
+      fill_in 'Password Confirmation', with: 'somethingdifferent'
+      click_button 'Sign Up'
 
       expect(page).to have_content("doesn't match")
-      expect(page).to_not have_content("Sign Out")
+      expect(page).to_not have_content('Sign Out')
     end
   end
 end
