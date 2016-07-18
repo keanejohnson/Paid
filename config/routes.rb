1f3ccdfd0e2606376f449e3bb50e4ca83ba23468
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-18 14:48:29 -0400

    Added a user sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "users#show"
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
