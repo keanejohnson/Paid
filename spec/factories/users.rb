da042ce6610421e992d7524e98ef53036ad22304
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-18 19:33:28 -0400

    user can now sign in

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,9 @@
 FactoryGirl.define do
   factory :user do
+    first_name "George"
+    last_name "Washington"
+    email "crossthedelaware@gmail.com"
+    password "independence"
+    password_confirmation "independence"
   end
 end
