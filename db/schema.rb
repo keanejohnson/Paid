52b9d193f8ee5642867cd96a553add0a114c9948
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-18 15:56:08 -0400

    hound errors

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160718173309) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -33,7 +32,8 @@ ActiveRecord::Schema.define(version: 20160718173309) do
     t.datetime 'updated_at',                          null: false
   end
 
-  add_index 'users', ['email'], name: 'index_users_on_email', unique: true, using: :btree
-  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
-
+  add_index 'users', ['email'], name: 'index_users_on_email',
+    unique: true, using: :btree
+  add_index 'users', ['reset_password_token'],
+    name: 'index_users_on_reset_password_token', unique: true, using: :btree
 end
