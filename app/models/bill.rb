d402704edc698d482cf071c1d1a74325901774b2
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-19 16:27:14 -0400

    add a bill model, controller, table

---
+++ app/models/bill.rb
@@ -0,0 +1,18 @@
+class Bill < ActiveRecord::Base
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :description,
+                length: {
+                  maximum: 140,
+                  message: "Shorter description, please!"
+                }
+  validates :amount,
+              presence: true,
+              numericality: true,
+              format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+  validates :party_size,
+              presence: true,
+              numericality: { only_integer: true, greater_than: 0 }
+  validates :user_id, presence: true, numericality: { only_integer: true }
+end
