800c42d3ae25a8a64c11e5089e39b1ee1af5a998
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-19 16:30:37 -0400

    fixed houn errors

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -8,11 +8,11 @@ class Bill < ActiveRecord::Base
                   message: "Shorter description, please!"
                 }
   validates :amount,
-              presence: true,
-              numericality: true,
-              format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+    presence: true,
+    numericality: true,
+    format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
   validates :party_size,
-              presence: true,
-              numericality: { only_integer: true, greater_than: 0 }
+    presence: true,
+    numericality: { only_integer: true, greater_than: 0 }
   validates :user_id, presence: true, numericality: { only_integer: true }
 end
