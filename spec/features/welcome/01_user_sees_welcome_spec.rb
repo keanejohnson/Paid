65da2d0f17f112aaeac4b90501b50c374040f0bc
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-24 15:54:25 -0400

    hound error

--- spec/features/welcome/01_user_sees_welcome_spec.rb
+++ spec/features/welcome/01_user_sees_welcome_spec.rb
@@ -5,7 +5,6 @@ feature 'user sees welcome page', %(
   I want to be able to see a welcome page
   So I can view something as the root path
 ) do
-
   context "as an unauthenticated user" do
     scenario "I see a welcome page when navigating to the root path" do
       visit root_path
