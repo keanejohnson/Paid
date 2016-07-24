dafb42b21ec6cc3a5824e5f909a3cdced2469473
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-24 15:42:10 -0400

    style and tests

---
+++ spec/features/welcome/01_user_sees_welcome_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature 'user sees welcome page', %(
+  As an unauthenticated user,
+  I want to be able to see a welcome page
+  So I can view something as the root path
+) do
+
+  context "as an unauthenticated user" do
+    scenario "I see a welcome page when navigating to the root path" do
+      visit root_path
+
+      expect(page).to have_content("PAID")
+      expect(page).to have_link("Sign Up")
+      expect(page).to have_link("Sign In")
+    end
+  end
+end
